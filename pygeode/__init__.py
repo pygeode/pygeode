f9d5ccd9c8ad5776c08400be8408dcc6e0bf5348
Peter Hitchcock <phitchcock@gmail.com>
2011-05-25 17:54:41 -0400

    plotsigmask is now imported from plot.py by default.

--- pygeode/__init__.py
+++ pygeode/__init__.py
@@ -52,8 +52,8 @@ from pygeode.ufunc import __all__ as ufunc_all
 __all__.extend(ufunc_all)
 
 # Plotting
-from pygeode.plot import plotvar, plotquiver
-__all__.extend(['plotvar', 'plotquiver'])
+from pygeode.plot import plotvar, plotquiver, plotsigmask
+__all__.extend(['plotvar', 'plotquiver', 'plotsigmask'])
 
 from pygeode.climat import *
 from pygeode.climat import __all__ as climat_all
