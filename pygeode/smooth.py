504da3dd17045cde820854a132f50692ab47f727
Peter Hitchcock <peterh@atmosp.physics.utoronto.ca>
2011-06-21 10:02:33 -0400

    Improved handling of endpoints

--- pygeode/smooth.py
+++ pygeode/smooth.py
@@ -12,8 +12,7 @@ class SmoothVar (Var):
     import numpy as np
 
     # Moved this here temporarily, otherwise pygeode won't start on sparc01 (no scipy available)
-    from scipy import signal as sg
-    ktypes = {'hann': sg.hann, 'boxcar': sg.boxcar, 'triang': sg.triang}
+    ktypes = {'hann': np.hanning, 'flat': np.ones, 'bartlett': np.bartlett, 'hamm': np.hamming}
 
     # Construct new variable
     self.saxis = saxis
@@ -25,20 +24,18 @@ class SmoothVar (Var):
 
     assert klen <= var.shape[saxis], 'Kernel must be shorter than dimension being smoothed.'
     self.klen = klen
-    self.kshape = [klen if i == saxis else 1 for i in range(var.naxes)]
 
-    # Normalize kernel
+    # Normalize and reshape kernel
     self.kernel /= np.sum(self.kernel)
+    self.kernel.shape = [klen if i == saxis else 1 for i in range(var.naxes)]
 
     Var.__init__(self, var.axes, var.dtype, name=var.name, atts=var.atts, plotatts=var.plotatts)
   # }}}
 
   def getview (self, view, pbar):
   # {{{
-    import numpy as np
-
-    # Moved this here temporarily, otherwise pygeode won't start on sparc01 (no scipy available)
     from scipy import signal as sg
+    import numpy as np
 
     saxis = self.saxis
 
@@ -46,22 +43,46 @@ class SmoothVar (Var):
     ind = view.integer_indices[saxis]
     st, sp = np.min(ind), np.max(ind)
 
-    # Extend view along smoothing axis if possible
-    offs = self.klen / 2
-    insl = slice(max(st - offs, 0), min(sp + offs, self.shape[saxis]), 1)
+    # Extend view along smoothing axis; use data past slice if present, otherwise mirror data
+    loffs = self.klen / 2
+    roffs = self.klen - loffs
+    src_shape = view.shape[:saxis] + (len(ind) + self.klen,) + view.shape[saxis+1:]
+    src = np.empty(src_shape, self.var.dtype)
+    
+    # Construct slices for mirroring pre-convolved data
+    ssli = [slice(None) for i in range(self.naxes)]
+    sslo = [slice(None) for i in range(self.naxes)]
+    mleft_len = max(loffs - st, 0)
+    mright_len = max(sp + roffs + 1, self.shape[saxis]) - self.shape[saxis]
+
+    # Construct slice into source variable
+    insl = slice(max(st - loffs, 0), min(sp + roffs, self.shape[saxis]), 1)
+    aview = view.modify_slice(saxis, insl)
 
     # Construct slice into convolved output
-    outsl = [ind - insl.start if i == saxis else slice(None) for i in range(self.naxes)]
+    outsl = [ind - (st - loffs) if i == saxis else slice(None) for i in range(self.naxes)]
 
     # Get source data
-    aview = view.modify_slice(saxis, insl)
-    src = aview.get(self.var, pbar=pbar)
+    sslo[saxis] = slice(mleft_len, self.shape[saxis] + self.klen - mright_len)
+    src[sslo] = aview.get(self.var, pbar=pbar)
+
+    # Mirror boundaries, if necessary
+    if mleft_len > 0:
+      ssli[saxis] = slice(2*mleft_len, mleft_len,-1)
+      sslo[saxis] = slice(0, mleft_len)
+      src[sslo] = src[ssli]
+
+    # Mirror boundaries, if necessary
+    if mright_len > 0:
+      ssli[saxis] = slice(-mright_len-2,-2*mright_len-2,-1)
+      sslo[saxis] = slice(-mright_len, None)
+      src[sslo] = src[ssli]
 
     # Return convolved array (forcing the dtype seems to be required)
-    return sg.convolve(src, self.kernel.reshape(self.kshape), 'same')[outsl].astype(self.dtype)
+    return sg.convolve(src, self.kernel, 'same', old_behavior=False)[outsl].astype(self.dtype)
   # }}}
 
 def smooth(var, saxis, klen=15, kernel='hann'):
-  if klen <= 1:
+  if klen <= 2:
     return var
   return SmoothVar(var, saxis=var.whichaxis(saxis), kernel=kernel, klen=klen)
