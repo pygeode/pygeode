17ce08fd273c9f19a1c79952c86467a47fb68445
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2011-07-15 12:13:56 -0400

    Re-added test for issue 15

---
+++ tests/issues/issue015_test.py
@@ -0,0 +1,20 @@
+# Issue 15 - netcdf_new crashes because of axes mismatch
+# http://code.google.com/p/pygeode/issues/detail?id=15
+
+def test_issue015():
+  from pygeode.formats import netcdf_new as nc
+  from pygeode.axis import Lat, Lon, Pres
+
+  # Create a simple variable
+  lat = Lat([75,85,95])
+  lon = Lon([100, 110, 120, 130])
+  pres = Pres([1000,900,800,700])
+  x = (lat-80)**2 + lon - pres/2.
+  x.name = "stuff"
+
+  # Save as a netcdf file
+  nc.save("issue015_test.nc", x)
+
+  # Reload
+  f = nc.open("issue015_test.nc")
+  y = f.stuff.load()
