1ec0d9d45e3ed569c8c5ece38bdfdc34704fcf1e
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2011-09-17 15:31:48 -0400

    Fixed names for issue025_test.
    --HG--
    branch : release-0.6

--- tests/issues/issue025_test.py
+++ tests/issues/issue025_test.py
@@ -5,19 +5,19 @@ from pygeode.formats import netcdf as nc
 from pygeode.axis import Lat
 from pygeode.var import Var
 
-def test_issueXXX():
+def test_issue025():
   lat = Lat([80,70,60])
   var = Var(axes=[lat], values=[1,2,3], name='2B')
 
   # Save the variable
-  nc.save ("issueXXX_test.nc", var)
+  nc.save ("issue025_test.nc", var)
 
   # This may crash in some versions of the netcdf library.
 
   # Even if it doesn't crash, it's a good idea to enforce the legal
   # netcdf names
 
-  f = nc.open("issueXXX_test.nc")
+  f = nc.open("issue025_test.nc")
 
   assert len(f.vars) == 1
   # Must not start with a digit (should have been filtered)
