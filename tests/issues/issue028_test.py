b1da2dcd421972aab1126ac0ee62e0109bc8be24
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2012-06-14 14:19:20 -0400

    Test case for issue 28.
    --HG--
    branch : release-0.6

---
+++ tests/issues/issue028_test.py
@@ -0,0 +1,12 @@
+# Issue 28 - can't mix complex scalars with PyGeode variables
+
+from pygeode.var import Var
+from pygeode.axis import Lat
+
+# Create a mock variable
+lat = Lat([-45.])
+var = Var(axes=[lat], values=[123.])
+
+# Try scaling it by a complex number
+var2 = var * 1j
+assert var2.dtype.name == 'complex128'
