2ee1f9bb2efb23fb4418e1f30f928925135cc936
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2012-06-15 11:19:23 -0400

    Prep for 0.6.2
    --HG--
    branch : release-0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 # PyGeode installation script
 
 setup (	name="pygeode",
-	version="0.6.1",
+	version="0.6.2",
         author="Peter Hitchcock, Andre Erler, Mike Neish",
         author_email="",
         url="http://sparc01.atmosp.physics.utoronto.ca/pygeode/docs",
