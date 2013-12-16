46acf36fb989334dcf46275ea5246ac5f40ea89d
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2013-12-16 14:45:19 -0500

    Need to explicitly add the numpy header location in setup.py.
    Otherwise, building fails for some distributions.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import numpy as np
 
 interpcore = Extension ('pygeode.interpcore', sources=['pygeode/interp.c'], libraries=['gsl','gslcblas'])
 timeaxiscore = Extension ('pygeode.timeaxiscore', sources=['pygeode/timeaxis.c'], extra_compile_args=['-std=c99'])
@@ -41,6 +42,7 @@ the underlying computations and to create plots.
         # in the 'pygeode' subdirectory.
 	package_data={'pygeode': ['*.dll'], 'pygeode.formats': ['*.dll']},
 	packages=["pygeode", "pygeode.formats", "pygeode.server", "pygeode.plugins", "pygeode.plot"],
+	include_dirs = [np.get_include()],
 	ext_modules=[interpcore, timeaxiscore, quadrulepy, toolscore, svdcore, eofcore, opendapcore, gribcore]
 )
 
