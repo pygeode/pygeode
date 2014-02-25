c1d79bbb82bc28803230359a29f2cfe58fa7fc6d
Peter Hitchcock <peterh@atmosp.physics.utoronto.ca>
2014-02-25 11:52:20 +0000

    Added 2nd derivative var

--- pygeode/deriv.py
+++ pygeode/deriv.py
@@ -96,5 +96,96 @@ class DerivativeVar (Var):
     return values
   # }}}
 
+class SecondDerivativeVar (Var):
+  '''Finds second derivative, using centred differences in the 
+    interior of the axis and one sided differences on the boundaries.'''
+
+  def __init__ (self, var, daxis, dx=None):
+  # {{{
+    ''' __init__()'''
+
+    from pygeode.var import Var
+
+    self.daxis = daxis = var.whichaxis(daxis)
+    assert var.shape[daxis] > 1, "need at least two values along differentiation axis"
+
+    if dx is not None:
+      if dx.naxes == 1:
+        assert dx.shape[0] == var.shape[daxis]
+        self.dx = dx.replace_axes(newaxes=(var.axes[daxis],))
+      else:
+        assert all([dx.hasaxis(a) for a in var.axes])
+        self.dx = dx
+    else:
+      self.dx = var.axes[daxis]
+
+    self.var = var
+
+    # Construct new variable
+
+    if var.name != '':
+      name = 'd2' + var.name
+    else:
+      name = 'd2(UnknownVar)'
+
+    Var.__init__(self, var.axes, var.dtype, name=name, atts=var.atts, plotatts=var.plotatts)
+  # }}}
+
+  def getview (self, view, pbar):
+  # {{{
+    from numpy import arange, min, max, clip
+    import numpy as np
+
+    daxis = self.daxis
+    Nd = self.shape[daxis]
+
+    # Get integer indices along the differentiation axis
+    ind = view.integer_indices[daxis]
+
+    # Want to do the finite difference with values to the left & right
+    left = ind-1
+    centre = ind
+    right = ind+1
+
+    # Truncate to the left & right boundaries
+    left[left==-1] = 2
+    right[right==Nd] = Nd-3
+
+    # All the points we need to request (unique occurrences only)
+    allpoints = np.union1d(left, np.union1d(centre, right))
+
+    allview = view.modify_slice(daxis, allpoints)
+
+    # Get the data and axis values for these points
+    allvalues = allview.get(self.var, pbar=pbar)
+    allaxis = allview.get(self.dx)
+
+    # Define a map from these unique points back to the left & right arrays
+    getleft = np.searchsorted(allpoints,left)
+    getcentre = np.searchsorted(allpoints,centre)
+    getright = np.searchsorted(allpoints,right)
+    # Make this 1D map into the right shape for the view (if multi-dimensional)
+    getleft = [slice(None)]*daxis + [getleft] + [slice(None)]*(self.naxes-daxis-1)
+    getcentre = [slice(None)]*daxis + [getcentre] + [slice(None)]*(self.naxes-daxis-1)
+    getright = [slice(None)]*daxis + [getright] + [slice(None)]*(self.naxes-daxis-1)
+
+    # Finally, get the left & right values, and do the finite difference
+    L = allvalues[getleft]
+    C = allvalues[getcentre]
+    R = allvalues[getright]
+    La = allaxis[getleft]
+    Ca = allaxis[getcentre]
+    Ra = allaxis[getright]
+
+    den = 2. / ((Ra - Ca) * (Ca - La) * (Ra - La))
+    dL = (Ra - Ca) * den
+    dC = (La - Ra) * den
+    dR = (Ca - La) * den
+    return np.asarray(dL * L + dC * C + dR * R, self.dtype)
+  # }}}
+
 def deriv(var, daxis, dx=None, df='centre'):
-  return DerivativeVar(var, var.whichaxis(daxis), dx=dx, df=df)
+  if df == '2':
+    return SecondDerivativeVar(var, var.whichaxis(daxis), dx=dx)
+  else:
+    return DerivativeVar(var, var.whichaxis(daxis), dx=dx, df=df)
