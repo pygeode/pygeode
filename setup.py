93ffcfb1d75d287408c1639922cb495012c09d37
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2014-04-14 12:13:32 -0400

    Prep for final release 1.0.0.
    --HG--
    branch : release-1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ gribcore = Extension ('pygeode.formats.gribcore', sources=['pygeode/formats/grib
 # PyGeode installation script
 
 setup (	name="python-pygeode",
-	version="1.0.0~beta1",
+	version="1.0.0",
         description = "Gridded data manipulator for Python",
 	long_description = """\
 PyGeode is a software library intended to simplify the management, analysis,
