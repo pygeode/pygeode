dabaeca8171bde8ab3e6acf0ddc487aa9d7bd023
Mike Neish <michael.neish@canada.ca>
2018-03-21 17:35:37 -0400

    Should now be able to use Travis-CI on Python3 builds.
    Note: numpy won't install on Python3 versions before 3.4.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,7 +5,7 @@ set -e -x
 yum install -y atlas-devel lapack-devel gsl-devel netcdf
 
 # Compile wheels
-for PYBIN in /opt/python/*27*/bin; do
+for PYBIN in /opt/python/{cp27,cp3[4-9]}*/bin; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel --no-deps /io/ -w wheelhouse/
 done
