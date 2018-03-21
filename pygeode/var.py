pygeode/pygeode

commit ddda52149c18053819b37fc426c58dd81c373dbd

Make a writeable copy of the data returned by Var.get().
Closes #96.

pygeode/var.py
@@ -714,8 +714,10 @@ class Var(object):
        259.81373805]]
     """
     from pygeode.view import View
+    import numpy as np
     var = self.__call__(**kwargs)
-    return View(var.axes).get(var,pbar=pbar)
+    data = View(var.axes).get(var,pbar=pbar)
+    return np.array(data,copy=True)
   # }}}
 
   def getweights (self, iaxes = None):

commit 9c22cd374014c9de9b871075b44ddf0c77f061b4

Fix a bug introduced by ddda52149c18053819b37fc426c58dd81c373dbd.
Don't need to invoke np.array() on scalar values, or they get promoted to arrays.

pygeode/var.py
@@ -717,7 +717,9 @@ class Var(object):
     import numpy as np
     var = self.__call__(**kwargs)
     data = View(var.axes).get(var,pbar=pbar)
-    return np.array(data,copy=True)
+    if isinstance(data,np.ndarray):
+      data = np.array(data,copy=True)
+    return data
   # }}}
 
   def getweights (self, iaxes = None):