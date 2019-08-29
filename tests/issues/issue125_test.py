b4a4c69ac45f47704b8722a3d5d49524d1b6acfe
Mike Neish <michael.neish@canada.ca>
2019-08-29 11:59:56 -0400

    Test for issue #125.

---
+++ tests/issues/issue125_test.py
@@ -0,0 +1,13 @@
+# Issue 125 - Issue with loading data into memory after converting Pygeode dataset to Xarray dataset
+# https://github.com/pygeode/pygeode/issues/125
+
+import pygeode as pyg
+import pygeode.ext_xarray as pyg_xr
+import xarray as xr
+from pygeode.tutorial import t1
+
+t1_xr = pyg_xr.to_xarray(t1)
+assert hasattr(t1_xr.Temp,'values')
+values = t1_xr.Temp.values
+assert (values.shape == t1.Temp.shape)
+
